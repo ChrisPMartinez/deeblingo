4bfb187cd69321ce94f3b0ecea38f73b010f57d6
Chris Martinez <32532090+ChrisPMartinez@users.noreply.github.com>
2022-12-20 08:24:41 -0700

    Updated API URL

--- js/deeblingo.js
+++ js/deeblingo.js
@@ -1,5 +1,4 @@
-var apiUrl = "https://5bcxmqtakg.execute-api.us-west-2.amazonaws.com/Prod/api/";
-//var apiUrl = "https://localhost:44315/api/"
+var apiUrl = "https://yourdeeblingoapiurl.tld"
 
 $(function(){
     $(".square").on('click', function(){
